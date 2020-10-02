1d7b17149187819c544a37fae8826a2ce86d8f23
Konrad Kukla <kondziocard@gmail.com>
2020-10-02 02:18:16 +0200

    Updates

---
+++ precache-manifest.334b00963a98865657e4d4246a2e9d9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6a3b17ffa5de76ea8accd64e9bcb4c",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "013a358c266b74d9d23b",
+    "url": "/quiz-app/static/js/2.eb65fa21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-app/static/js/2.eb65fa21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05903cd61519e74dbd13",
+    "url": "/quiz-app/static/js/main.0def6178.chunk.js"
+  },
+  {
+    "revision": "e15a1632907c25963bb7",
+    "url": "/quiz-app/static/js/runtime-main.4f483d45.js"
+  },
+  {
+    "revision": "fbfcd3be0fef3afa5e32eab8929b2765",
+    "url": "/quiz-app/static/media/robert-katzki.fbfcd3be.jpg"
+  }
+]);
